4b0c73d735122a70605d3a1e27b2c4703272e1fd
Trard <trardone@gmail.com>
2026-02-28 23:21:50 +0300

    add base user

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class User(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
