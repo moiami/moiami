e2f781c9af4a2824dda8fc4b2aaa127cecd14409
Trard <trardone@gmail.com>
2026-03-01 22:19:23 +0300

    add actions

--- src/apps/actions/models.py
+++ src/apps/actions/models.py
@@ -1,8 +1,26 @@
+from django.conf import settings
+from django.db.models import (
+    CASCADE,
+    PROTECT,
+    CharField,
+    DateTimeField,
+    ForeignKey,
+    Model,
+)
 
-# Create your models here
 
-# class Action(models.Model):
-#     pass
+class ActionType(Model):
+    name = CharField(max_length=255)
 
-# class ActionType(models.Model):
-#     pass
+class Action(Model):
+    happened_at = DateTimeField()
+    user = ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=CASCADE,
+        related_name='actions',
+    )
+    type = ForeignKey(
+        ActionType,
+        on_delete=PROTECT,
+        related_name='actions',
+    )
