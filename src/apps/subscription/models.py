b41486c75669a72661d583026aa2e3f52a0579c1
acyushka <izotovarsenii3005@gmail.com>
2026-03-01 21:46:03 +0300

    Fix subscription models

--- src/apps/subscription/models.py
+++ src/apps/subscription/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Subscription(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         ordering = ('price',)
@@ -26,7 +26,6 @@ class UserSubscription(models.Model):
         related_name='user_subscriptions'
     )
     expired_at = models.DateTimeField()
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.user.id} - {self.subscription.name}"
