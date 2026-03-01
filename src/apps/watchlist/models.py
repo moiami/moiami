55368d305cbd2f4ea3b401b058370b31a50b4118
Trard <trardone@gmail.com>
2026-03-01 22:03:18 +0300

    add watchlist

--- src/apps/watchlist/models.py
+++ src/apps/watchlist/models.py
@@ -1,5 +1,19 @@
+import uuid
+
+from django.db import models
+from django.db.models import (
+    CharField,
+    ManyToManyField,
+    UUIDField,
+)
+
+from apps.catalog.models import Movie
 
 
-# Create your models here.
-# class WatchList(models.Model):
-#     pass
+class WatchList(models.Model):
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    name = CharField(max_length=255)
+    movies = ManyToManyField(Movie, related_name='watchlists', blank=True)
+
+    def __str__(self):
+        return str(self.name) + str(self.id)
