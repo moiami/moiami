c8ed3090ad56957909bffb47ab70344e176b12e7
semayagi <yagid.semen07.07@yandex.ru>
2026-03-04 23:40:42 +0300

    User model ready

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,7 +1,27 @@
+import uuid
 from django.db import models
+from watchlist.models import WatchList 
+from subscription.models import UserSubscription
 
-# Create your models here
+from django.db.models import (
+    CASCADE,
+    CharField,
+    DateField,
+    IntegerField,
+    ManyToManyField,
+    OneToOneField,
+    TextField,
+    URLField,
+    UUIDField,
+)
 
 class User(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=26)
+    password = models.CharField(max_length=128)  # Django hashes are typically 128 chars
+    isAdmin = models.BooleanField(default=False)
+    watchlists = ManyToManyField(WatchList, related_name='watchlists', blank=True)
+    subscriptions = ManyToManyField(UserSubscription, related_name='subscriptions', blank=True)
+
+    def __str__(self):
+        return str(self.name) + str(self.id)
