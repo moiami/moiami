b9384a6dec730b078e71a11a5d5ca600637a040c
Trard <trardone@gmail.com>
2026-03-20 12:07:36 +0300

    remove errors

--- domain/errors/invalid_json.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-
-def invalid_json_error() -> JsonResponse:
-    return JsonResponse(
-        {'description': 'Invalid JSON'},
-        status=400,
-    )
--- domain/errors/not_authenticated.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-
-def not_authenticated_error() -> JsonResponse:
-    return JsonResponse(
-        {'description': 'Not authenticated'},
-        status=401,
-    )
